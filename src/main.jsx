7a0fd7755cdfa2e9da9de9406a6583decb0421fc
petropoulosstergios5-blip <petropoulosstergios5@gmail.com>
2026-09-02 22:33:22 +0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TaxiFleetApp from './TaxiFleetApp.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TaxiFleetApp />
-  </React.StrictMode>
-);
